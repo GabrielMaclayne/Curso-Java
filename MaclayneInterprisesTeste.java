31f4c6b443a1d00793d30250269bc64171b39bc4
GabrielMaclayne <148655629+GabrielMaclayne@users.noreply.github.com>
2023-10-21 23:22:39 -0300

    Add files via upload

---
+++ MaclayneInterprisesTeste.java
@@ -0,0 +1,5 @@
+class Maclayne_Interprises {
+	public static void main (String[] args){
+	  System.out.println("Assistência Técnica especializada em celulares e computadores. Instagram da empresa: @maclayne_interprises");
+	}
+}
